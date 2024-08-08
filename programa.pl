e6ad3fb00b3d02ffa9df6230ab9d47f8876bb294
Juan Cruz <149621806+Juanter134@users.noreply.github.com>
2024-08-08 09:37:34 -0300

    En progreso.pl

--- programa.pl
+++ programa.pl
@@ -1,1 +1,35 @@
-% Aquí va el código.
+% Parcial Parque de atracciones %
+persona(nina, 22, 1.60).
+persona(marcos, 8, 1.32).
+persona(osvaldo, 13, 1.29).
+
+puedeSubirse(tren_fantasma, Persona):-
+    persona(Persona, Edad, _),
+    Edad >= 12.
+puedeSubirse(montania_rusa, Persona):-
+    persona(Persona, _, Altura),
+    Altura > 1.30.
+puedeSubirse(maquina_tiquetera, Persona):-
+    persona(Persona, _, _).
+
+puedeSubirse(tobogan_gigante, Persona):-
+    persona(Persona, _, Altura),
+    Altura >= 1.50.
+puedeSubirse(rio_lento, Persona):-
+    persona(Persona, _, _).
+puedeSubirse(maremoto, Persona):-
+    persona(Persona, Edad, _),
+    Edad >= 5.
+
+atraccion(Atraccion):-
+    puedeSubirse(Atraccion, _).
+
+parque(parque_acuatico, [tobogan_gigante, rio_lento, maremoto]).
+parque(parque_de_atracciones, [tren_fantasma, montania_rusa, maquina_tiquetera]).
+
+esParaElle(Persona, Parque):-
+    persona(Persona, _, _),
+    parque(Parque, Atracciones),
+    forall(member(Atraccion, Atracciones), puedeSubirse(Atraccion, Persona)).
+
+programa(parque_acuatico, [tobogan_gigante, maremoto, rio_lento]).
